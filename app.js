0797838c4b08071b9611265d53bc2aab5ad04e59
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-02-25 14:54:38 -0600

    Storybooks nodejs app

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars Middleware
+app.engine('.hbs', engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
