e762bdb0f93eba1fc5c0886059ebd266995a1fc8
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-03-02 14:39:38 -0600

    fixed a bit of confilicts with mongoose version

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-const express = require ('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 // @desc Login/Landing page
 // @route GET /
-router.get('/', (req, res) => {
-    res.render('login')
-})
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login");
+});
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
-})
+router.get("/dashboard", ensureAuth, (req, res) => {
+  res.render("dashboard");
+});
 
-module.exports = router
+module.exports = router;
