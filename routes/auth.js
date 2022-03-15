b378899116cee9fd0b1d259f2bae57a1112b86f2
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-03-15 15:30:04 -0500

    added the auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,26 @@
-const express = require ('express')
-const res = require('express/lib/response')
-const passport = require('passport')
-const router = express.Router()
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
 
-// @desc Auth with Google
-// @route GET /auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
-// @desc Google auth callback
-// @route GET /auth/google/callback
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/dashboard')
-})
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/dashboard");
+  }
+);
 
-// @desc Logout user
-// @route /auth/logout
+// @desc    Logout user
+// @route   /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-router.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/')
-})
-
-module.exports = router
+module.exports = router;
